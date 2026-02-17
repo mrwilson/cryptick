e8615be4861bbd2258fdd1f980dd35a65d7908c1
Alex Wilson <gpg@probablyfine.co.uk>
2026-02-17 14:10:05 +0000

    feat: Generate about.html with base template

--- metalsmith.js
+++ metalsmith.js
@@ -1,11 +1,21 @@
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import Metalsmith from 'metalsmith';
+import layouts from '@metalsmith/layouts';
 
 Metalsmith(dirname(fileURLToPath(import.meta.url)))
     .clean(true)
     .source('./src')
     .destination('./build')
+    .use(
+        layouts({
+            transform: 'handlebars',
+            pattern: 'about.html',
+            default: 'base.html',
+            extName: false,
+        }),
+    )
+    .env('DEBUG', '@metalsmith/layouts*')
     .build((err) => {
         if (err) throw err;
     });
