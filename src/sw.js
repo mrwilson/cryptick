e1ed2b689b803c73d7a58577e133b156b948d76a
Alex Wilson <gpg@probablyfine.co.uk>
2026-02-24 20:24:41 +0000

    feat: Add offline capability with service worker

---
+++ src/sw.js
@@ -0,0 +1,55 @@
+const files = [
+    '/index.html',
+    '/create.html',
+    '/clue.html',
+    '/error.html',
+    '/base.css',
+
+    '/error.svg',
+    '/logo.svg',
+
+    '/miniclue/decoder.js',
+    '/miniclue/create.js',
+    '/miniclue/errors.js',
+    '/miniclue/utils.js',
+    '/miniclue/clue.js',
+    '/miniclue/examples.js',
+
+    '/robots.txt',
+    '/manifest.json',
+    '/.well-known/assetlinks.json',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open('cryptick');
+            await cache.addAll(files);
+        })(),
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    const request = event.request;
+
+    if (!request.url.startsWith('http:') && !request.url.startsWith('https:')) {
+        return;
+    }
+
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        (async () => {
+            const fromCache = await caches.match(request);
+            if (fromCache) {
+                return fromCache;
+            }
+            const response = await fetch(request);
+            const cache = await caches.open('cryptick');
+            await cache.put(request, response.clone());
+            return response;
+        })(),
+    );
+});
