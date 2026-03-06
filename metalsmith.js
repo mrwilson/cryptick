fb17537f1cbe5d23503e71d793a47fb45c6fb735
Alex Wilson <gpg@probablyfine.co.uk>
2026-03-06 17:00:30 +0000

    spike: First cut of RSS feed

--- metalsmith.js
+++ metalsmith.js
@@ -3,14 +3,27 @@ import { fileURLToPath } from 'node:url';
 import { execSync as exec } from 'node:child_process';
 import Metalsmith from 'metalsmith';
 import layouts from '@metalsmith/layouts';
+import inPlace from '@metalsmith/in-place';
 import cotd from './cotd.json' with { type: 'json' };
 import { ClueDecoder } from './src/miniclue/decoder.js';
 
 const commitHash = exec('git rev-parse HEAD', { encoding: 'utf-8' }).trim();
 
-const clueOfTheDay = cotd[new Date().toISOString().slice(0, 10)];
+const decoder = new ClueDecoder({});
 
-const clue = new ClueDecoder({}).decode(`#${clueOfTheDay.hash}`);
+const allClues = Object.keys(cotd).map((date) => {
+    let clue = cotd[date];
+    let decodedClue = decoder.decode('#' + clue.hash);
+    return {
+        author_name: clue.author.name,
+        author_link: clue.author.link,
+        clue: `${decodedClue.clue} (${decodedClue.enumeration})`,
+        link: `/clue.html#${clue.hash}`,
+        date: date,
+    };
+});
+
+const today = new Date().toISOString().slice(0, 10);
 
 Metalsmith(dirname(fileURLToPath(import.meta.url)))
     .clean(true)
@@ -20,20 +33,21 @@ Metalsmith(dirname(fileURLToPath(import.meta.url)))
         commitHash: commitHash,
         commitHash_Short: commitHash.substring(0, 8),
         builtAt: new Date().toISOString(),
-        cotd: {
-            author: {
-                name: clueOfTheDay.author.name,
-                link: clueOfTheDay.author.link,
-            },
-            clue: `${clue.clue} (${clue.enumeration})`,
-            link: `./clue.html#${clueOfTheDay.hash}`,
-        },
+        cotd: allClues.find((clue) => clue.date === today),
+        clues: allClues.filter((clue) => clue.date <= today),
     })
     .use(function original_filename(files) {
         Object.keys(files).forEach((file) => {
             files[file].original_filename = `src/${file}`;
         });
     })
+    .use(
+        inPlace({
+            transform: 'handlebars',
+            pattern: '*.xml',
+            extname: '*.xml',
+        }),
+    )
     .use(
         layouts({
             transform: 'handlebars',
