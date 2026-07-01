353dab8f3d066cf71a6bb63b0ac4696d22d23813
Alex Wilson <gpg@probablyfine.co.uk>
2026-07-01 09:37:17 +0100

    fix(download-stats): Move to new stats API

--- lib/download-stats.sh
+++ lib/download-stats.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 DAY=$1
-URL=https://plausible.io/cryptick.wildvale.co.uk/export
-QUERY="period=168h&date=${DAY}&filters=%5B%5B%22is%22%2C%22event%3Agoal%22%2C%5B%22Clue%3A%3ASolved%22%5D%5D%5D&interval=day&comparison=undefined"
-
+URL=https://plausible.io/api/stats/cryptick.wildvale.co.uk/export/
 mkdir -p data/
 
-curl -fsSL "${URL}?${QUERY}" -o temp.zip || { echo "Download failed"; exit 1; }
-unzip -p temp.zip pages.csv > data/pages.csv || { echo "Extraction failed"; rm temp.zip; exit 1; }
+curl "${URL}" -H 'accept: */*' -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1' \
+  --data-raw "{
+    \"date_range\":\"30d\",
+    \"relative_date\":\"${DAY}\",
+    \"filters\":[[\"is\",\"event:goal\",[\"Clue::Solved\"]]],
+    \"include\":{\"imports\": true},
+    \"reports\":{
+      \"pages.csv\":{
+        \"dimensions\":[\"event:page\"],
+        \"metrics\":[\"visitors\",\"group_conversion_rate\"]
+       }
+      }
+    }" -o data/temp.zip || { echo "Download failed"; exit 1; }
 
-rm -f temp.zip
+unzip -p data/temp.zip pages.csv > data/pages.csv || { echo "Extraction failed"; rm data/temp.zip; exit 1; }
+
+rm -f data/temp.zip
